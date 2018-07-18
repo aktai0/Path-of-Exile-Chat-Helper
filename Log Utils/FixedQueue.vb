cae7b00b142e665c4022e90b771cff8451233633
aktai0 <xtoken.x@gmail.com>
2018-07-17 19:46:50 -0700

    Added FixedQueue type

---
+++ Log Utils/FixedQueue.vb
@@ -0,0 +1,16 @@
+﻿Public Class FixedQueue(Of T)
+   Inherits Queue(Of T)
+   Private QueueSize As Integer
+
+   Public Sub New(ByVal capacity As Integer)
+      MyBase.New(capacity)
+      QueueSize = capacity
+   End Sub
+
+   Public Shadows Sub Enqueue(ByVal val As T)
+      If Me.Count >= QueueSize Then
+         Me.Dequeue()
+      End If
+      MyBase.Enqueue(val)
+   End Sub
+End Class
