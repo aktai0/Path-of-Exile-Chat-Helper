6f84a7f2b862bac4598ca7a083ace5b430815522
aktai0 <xtoken.x@gmail.com>
2018-09-11 02:37:19 -0700

    Added guild/name/chat type filters, rearranged layout to dock

--- POE Chat Helper/MainWindow.Designer.vb
+++ POE Chat Helper/MainWindow.Designer.vb
@@ -24,59 +24,211 @@ Partial Class MainWindow
    Private Sub InitializeComponent()
       Me.ListBox1 = New System.Windows.Forms.ListBox()
       Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
-      Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+      Me.MessageRichTextBox = New System.Windows.Forms.RichTextBox()
+      Me.NameRichTextBox = New System.Windows.Forms.RichTextBox()
+      Me.GuildRichTextBox = New System.Windows.Forms.RichTextBox()
+      Me.FiltersGroupBox = New System.Windows.Forms.GroupBox()
+      Me.WhisperRadioButton = New System.Windows.Forms.RadioButton()
+      Me.PartyRadioButton = New System.Windows.Forms.RadioButton()
+      Me.GuildRadioButton = New System.Windows.Forms.RadioButton()
+      Me.TradeRadioButton = New System.Windows.Forms.RadioButton()
+      Me.GlobalRadioButton = New System.Windows.Forms.RadioButton()
+      Me.AnyRadioButton = New System.Windows.Forms.RadioButton()
+      Me.Label3 = New System.Windows.Forms.Label()
+      Me.Label2 = New System.Windows.Forms.Label()
       Me.Label1 = New System.Windows.Forms.Label()
+      Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+      Me.FiltersGroupBox.SuspendLayout()
       Me.SuspendLayout()
       '
       'ListBox1
       '
+      Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Fill
       Me.ListBox1.FormattingEnabled = True
       Me.ListBox1.ItemHeight = 16
-      Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+      Me.ListBox1.Location = New System.Drawing.Point(10, 10)
       Me.ListBox1.Name = "ListBox1"
-      Me.ListBox1.Size = New System.Drawing.Size(776, 452)
-      Me.ListBox1.TabIndex = 0
+      Me.ListBox1.Size = New System.Drawing.Size(1095, 466)
+      Me.ListBox1.TabIndex = 3
+      Me.ListBox1.TabStop = False
       '
       'BackgroundWorker1
       '
       Me.BackgroundWorker1.WorkerReportsProgress = True
       '
-      'RichTextBox1
+      'MessageRichTextBox
       '
-      Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-      Me.RichTextBox1.Location = New System.Drawing.Point(52, 470)
-      Me.RichTextBox1.Multiline = False
-      Me.RichTextBox1.Name = "RichTextBox1"
-      Me.RichTextBox1.Size = New System.Drawing.Size(146, 22)
-      Me.RichTextBox1.TabIndex = 1
-      Me.RichTextBox1.Text = ""
+      Me.MessageRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+      Me.MessageRichTextBox.Location = New System.Drawing.Point(108, 21)
+      Me.MessageRichTextBox.Multiline = False
+      Me.MessageRichTextBox.Name = "MessageRichTextBox"
+      Me.MessageRichTextBox.Size = New System.Drawing.Size(221, 22)
+      Me.MessageRichTextBox.TabIndex = 0
+      Me.MessageRichTextBox.Text = ""
+      '
+      'NameRichTextBox
+      '
+      Me.NameRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+      Me.NameRichTextBox.Location = New System.Drawing.Point(108, 49)
+      Me.NameRichTextBox.Multiline = False
+      Me.NameRichTextBox.Name = "NameRichTextBox"
+      Me.NameRichTextBox.Size = New System.Drawing.Size(221, 22)
+      Me.NameRichTextBox.TabIndex = 1
+      Me.NameRichTextBox.Text = ""
+      '
+      'GuildRichTextBox
+      '
+      Me.GuildRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+      Me.GuildRichTextBox.Location = New System.Drawing.Point(108, 77)
+      Me.GuildRichTextBox.Multiline = False
+      Me.GuildRichTextBox.Name = "GuildRichTextBox"
+      Me.GuildRichTextBox.Size = New System.Drawing.Size(221, 22)
+      Me.GuildRichTextBox.TabIndex = 2
+      Me.GuildRichTextBox.Text = ""
+      '
+      'FiltersGroupBox
+      '
+      Me.FiltersGroupBox.Controls.Add(Me.WhisperRadioButton)
+      Me.FiltersGroupBox.Controls.Add(Me.PartyRadioButton)
+      Me.FiltersGroupBox.Controls.Add(Me.GuildRadioButton)
+      Me.FiltersGroupBox.Controls.Add(Me.TradeRadioButton)
+      Me.FiltersGroupBox.Controls.Add(Me.GlobalRadioButton)
+      Me.FiltersGroupBox.Controls.Add(Me.AnyRadioButton)
+      Me.FiltersGroupBox.Controls.Add(Me.Label3)
+      Me.FiltersGroupBox.Controls.Add(Me.Label2)
+      Me.FiltersGroupBox.Controls.Add(Me.Label1)
+      Me.FiltersGroupBox.Controls.Add(Me.MessageRichTextBox)
+      Me.FiltersGroupBox.Controls.Add(Me.GuildRichTextBox)
+      Me.FiltersGroupBox.Controls.Add(Me.NameRichTextBox)
+      Me.FiltersGroupBox.Dock = System.Windows.Forms.DockStyle.Bottom
+      Me.FiltersGroupBox.Location = New System.Drawing.Point(10, 476)
+      Me.FiltersGroupBox.Name = "FiltersGroupBox"
+      Me.FiltersGroupBox.Size = New System.Drawing.Size(1095, 113)
+      Me.FiltersGroupBox.TabIndex = 4
+      Me.FiltersGroupBox.TabStop = False
+      Me.FiltersGroupBox.Text = "Filters"
+      '
+      'WhisperRadioButton
+      '
+      Me.WhisperRadioButton.AutoSize = True
+      Me.WhisperRadioButton.Location = New System.Drawing.Point(424, 75)
+      Me.WhisperRadioButton.Name = "WhisperRadioButton"
+      Me.WhisperRadioButton.Size = New System.Drawing.Size(81, 21)
+      Me.WhisperRadioButton.TabIndex = 11
+      Me.WhisperRadioButton.Text = "Whisper"
+      '
+      'PartyRadioButton
+      '
+      Me.PartyRadioButton.AutoSize = True
+      Me.PartyRadioButton.Location = New System.Drawing.Point(424, 47)
+      Me.PartyRadioButton.Name = "PartyRadioButton"
+      Me.PartyRadioButton.Size = New System.Drawing.Size(62, 21)
+      Me.PartyRadioButton.TabIndex = 10
+      Me.PartyRadioButton.Text = "Party"
+      '
+      'GuildRadioButton
+      '
+      Me.GuildRadioButton.AutoSize = True
+      Me.GuildRadioButton.Location = New System.Drawing.Point(424, 19)
+      Me.GuildRadioButton.Name = "GuildRadioButton"
+      Me.GuildRadioButton.Size = New System.Drawing.Size(62, 21)
+      Me.GuildRadioButton.TabIndex = 9
+      Me.GuildRadioButton.Text = "Guild"
+      '
+      'TradeRadioButton
+      '
+      Me.TradeRadioButton.AutoSize = True
+      Me.TradeRadioButton.Location = New System.Drawing.Point(344, 75)
+      Me.TradeRadioButton.Name = "TradeRadioButton"
+      Me.TradeRadioButton.Size = New System.Drawing.Size(67, 21)
+      Me.TradeRadioButton.TabIndex = 8
+      Me.TradeRadioButton.Text = "Trade"
+      '
+      'GlobalRadioButton
+      '
+      Me.GlobalRadioButton.AutoSize = True
+      Me.GlobalRadioButton.Location = New System.Drawing.Point(344, 47)
+      Me.GlobalRadioButton.Name = "GlobalRadioButton"
+      Me.GlobalRadioButton.Size = New System.Drawing.Size(70, 21)
+      Me.GlobalRadioButton.TabIndex = 7
+      Me.GlobalRadioButton.Text = "Global"
+      '
+      'AnyRadioButton
+      '
+      Me.AnyRadioButton.AutoSize = True
+      Me.AnyRadioButton.Checked = True
+      Me.AnyRadioButton.Location = New System.Drawing.Point(344, 19)
+      Me.AnyRadioButton.Name = "AnyRadioButton"
+      Me.AnyRadioButton.Size = New System.Drawing.Size(53, 21)
+      Me.AnyRadioButton.TabIndex = 6
+      Me.AnyRadioButton.TabStop = True
+      Me.AnyRadioButton.Text = "Any"
+      '
+      'Label3
+      '
+      Me.Label3.AutoSize = True
+      Me.Label3.Location = New System.Drawing.Point(13, 77)
+      Me.Label3.Name = "Label3"
+      Me.Label3.Size = New System.Drawing.Size(70, 17)
+      Me.Label3.TabIndex = 5
+      Me.Label3.Text = "Guild Tag"
+      '
+      'Label2
+      '
+      Me.Label2.AutoSize = True
+      Me.Label2.Location = New System.Drawing.Point(13, 49)
+      Me.Label2.Name = "Label2"
+      Me.Label2.Size = New System.Drawing.Size(79, 17)
+      Me.Label2.TabIndex = 4
+      Me.Label2.Text = "Char Name"
       '
       'Label1
       '
       Me.Label1.AutoSize = True
-      Me.Label1.Location = New System.Drawing.Point(12, 470)
+      Me.Label1.Location = New System.Drawing.Point(13, 21)
       Me.Label1.Name = "Label1"
-      Me.Label1.Size = New System.Drawing.Size(43, 17)
-      Me.Label1.TabIndex = 2
-      Me.Label1.Text = "Filter:"
+      Me.Label1.Size = New System.Drawing.Size(65, 17)
+      Me.Label1.TabIndex = 3
+      Me.Label1.Text = "Message"
+      '
+      'FlowLayoutPanel1
+      '
+      Me.FlowLayoutPanel1.Location = New System.Drawing.Point(18, 18)
+      Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+      Me.FlowLayoutPanel1.Size = New System.Drawing.Size(200, 100)
+      Me.FlowLayoutPanel1.TabIndex = 5
       '
       'MainWindow
       '
       Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
       Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-      Me.ClientSize = New System.Drawing.Size(800, 573)
-      Me.Controls.Add(Me.RichTextBox1)
-      Me.Controls.Add(Me.Label1)
+      Me.ClientSize = New System.Drawing.Size(1115, 599)
       Me.Controls.Add(Me.ListBox1)
+      Me.Controls.Add(Me.FlowLayoutPanel1)
+      Me.Controls.Add(Me.FiltersGroupBox)
       Me.Name = "MainWindow"
+      Me.Padding = New System.Windows.Forms.Padding(10)
       Me.Text = "POE Chat Helper"
+      Me.FiltersGroupBox.ResumeLayout(False)
+      Me.FiltersGroupBox.PerformLayout()
       Me.ResumeLayout(False)
-      Me.PerformLayout()
 
    End Sub
 
    Friend WithEvents ListBox1 As ListBox
    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
-   Friend WithEvents RichTextBox1 As RichTextBox
+   Friend WithEvents MessageRichTextBox As RichTextBox
+   Friend WithEvents NameRichTextBox As RichTextBox
+   Friend WithEvents GuildRichTextBox As RichTextBox
+   Friend WithEvents FiltersGroupBox As GroupBox
+   Friend WithEvents Label3 As Label
+   Friend WithEvents Label2 As Label
    Friend WithEvents Label1 As Label
+   Friend WithEvents WhisperRadioButton As RadioButton
+   Friend WithEvents PartyRadioButton As RadioButton
+   Friend WithEvents GuildRadioButton As RadioButton
+   Friend WithEvents TradeRadioButton As RadioButton
+   Friend WithEvents GlobalRadioButton As RadioButton
+   Friend WithEvents AnyRadioButton As RadioButton
+   Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
 End Class
--- POE Chat Helper/MainWindow.vb
+++ POE Chat Helper/MainWindow.vb
@@ -1,4 +1,6 @@
-﻿Public Class MainWindow
+﻿Imports System.Text.RegularExpressions
+
+Public Class MainWindow
    Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
       Dim a = Log_Utils.LogScanner.Instance
       BackgroundWorker1.RunWorkerAsync()
@@ -19,26 +21,155 @@
    End Sub
 
    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+      Dim newLines As New List(Of Log_Utils.LogLine)(Log_Utils.LogScanner.Instance.ChatLines)
+
+      If MessageRichTextBox.Text <> "" Then
+         newLines = FilterBy(newLines, FilterType.Message)
+      End If
+
+      If NameRichTextBox.Text <> "" Then
+         newLines = FilterBy(newLines, FilterType.Name)
+      End If
+
+      If GuildRichTextBox.Text <> "" Then
+         newLines = FilterBy(newLines, FilterType.Guild)
+      End If
+
+      If Not AnyRadioButton.Checked Then
+         newLines = FilterBy(newLines, FilterType.ChatType)
+      End If
+
       ListBox1.BeginUpdate()
       ListBox1.Items.Clear()
-      Dim a = Log_Utils.LogScanner.Instance
-      For Each ll In a.ChatLines
-         If RichTextBox1.Text <> "" Then
-            ' Filter
-            If ll.Message.ToLower.Contains(RichTextBox1.Text.ToLower) Then
-               ListBox1.Items.Add(ll.ToString())
-            End If
-         Else
-            ' No filter
-            ListBox1.Items.Add(ll.ToString())
-         End If
+
+      ' Dim a = newLines.Select(Of String)(Function(x) As String
+      'Return x.ToString
+      'End Function)
+
+      For Each l In newLines
+         ListBox1.Items.Add(l.ToString)
       Next
+
+      'ListBox1.DataSource = a.ToList
+
       ListBox1.TopIndex = ListBox1.Items.Count - 1
       ListBox1.EndUpdate()
       FilterChanged = False
    End Sub
 
-   Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+   Private Enum FilterType
+      Message
+      Name
+      Guild
+      ChatType
+      'Guild, etc
+   End Enum
+
+   Private Function FilterBy(ByVal lineEnumerable As IEnumerable(Of Log_Utils.LogLine), ByVal filterType As FilterType) As IEnumerable(Of Log_Utils.LogLine)
+      Dim toReturn As New List(Of Log_Utils.LogLine)
+
+      For Each chatLine In lineEnumerable
+         Select Case filterType
+            Case FilterType.Message
+               If chatLine.Message.ToLower.Contains(MessageRichTextBox.Text.ToLower) Then
+                  toReturn.Add(chatLine)
+               End If
+            Case FilterType.Name
+               If chatLine.Character.ToLower.Contains(NameRichTextBox.Text.ToLower) Then
+                  toReturn.Add(chatLine)
+               End If
+            Case FilterType.Guild
+               If chatLine.GuildTag.ToLower.Contains(GuildRichTextBox.Text.ToLower) Then
+                  toReturn.Add(chatLine)
+               End If
+            Case FilterType.ChatType
+               Dim chatType = GetChatTypeFilter()
+
+               Select Case chatType
+                  Case Log_Utils.LogLine.ChatEnum.Global_, Log_Utils.LogLine.ChatEnum.Guild, Log_Utils.LogLine.ChatEnum.Local, Log_Utils.LogLine.ChatEnum.Party, Log_Utils.LogLine.ChatEnum.Trade
+                     If chatLine.ChatType = chatType Then
+                        toReturn.Add(chatLine)
+                     End If
+                  Case Log_Utils.LogLine.ChatEnum.WhisperIn
+                     If chatLine.ChatType = chatType OrElse chatLine.ChatType = Log_Utils.LogLine.ChatEnum.WhisperOut Then
+                        toReturn.Add(chatLine)
+                     End If
+                  Case Else
+                     toReturn.Add(chatLine)
+               End Select
+         End Select
+      Next
+
+      Return toReturn
+   End Function
+
+   Private Sub Filters_TextChanged(sender As Object, e As EventArgs) Handles MessageRichTextBox.TextChanged, NameRichTextBox.TextChanged, GuildRichTextBox.TextChanged
       FilterChanged = True
    End Sub
+
+
+   ' Focus stuff to select all text in a RTB on focus change, the mouse stuff is for focus change on click which is weird
+   Dim FocusChanged = False
+   Private Sub Filters_FocusChanged(sender As Object, e As EventArgs) Handles MessageRichTextBox.GotFocus, NameRichTextBox.GotFocus, GuildRichTextBox.GotFocus
+      Dim myRTB = CType(sender, RichTextBox)
+      myRTB.SelectAll()
+      FocusChanged = True
+      'Console.WriteLine("FocusChanged + " & myRTB.Text)
+   End Sub
+
+   Private Sub MessageRichTextBox_MouseClick(sender As Object, e As MouseEventArgs) Handles MessageRichTextBox.MouseClick, NameRichTextBox.MouseClick, GuildRichTextBox.MouseClick
+      Dim myRTB = CType(sender, RichTextBox)
+      'Console.WriteLine("MouseClick + " & myRTB.Text)
+      If FocusChanged Then
+         FocusChanged = Not FocusChanged
+         myRTB.SelectAll()
+      End If
+   End Sub
+
+   Private Sub ListBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles ListBox1.KeyUp
+      If e.Control And e.KeyCode = Keys.C Then
+         My.Computer.Clipboard.SetText(ListBox1.SelectedItem)
+      ElseIf e.Control And e.KeyCode = Keys.T Then
+         Dim result = ParseURL(ListBox1.SelectedItem)
+         If result <> "" Then
+            Process.Start(result)
+         End If
+      End If
+   End Sub
+
+   Private Function ParseURL(ByVal msg As String) As String
+      Dim pattern = "^.*(https?://[^ ]+).*$"
+      Dim regex = New Regex(pattern)
+      Dim result = regex.Match(msg)
+      Return result.Groups(1).Value
+   End Function
+
+   Private Sub ListBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseDoubleClick
+      Dim result = ParseURL(ListBox1.SelectedItem)
+      If result <> "" Then
+         Process.Start(result)
+      End If
+   End Sub
+
+   Private Sub RadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles AnyRadioButton.CheckedChanged, GlobalRadioButton.CheckedChanged, GuildRadioButton.CheckedChanged, PartyRadioButton.CheckedChanged, TradeRadioButton.CheckedChanged, WhisperRadioButton.CheckedChanged
+      FilterChanged = True
+   End Sub
+
+   Private Function GetChatTypeFilter() As Log_Utils.LogLine.ChatEnum
+      If AnyRadioButton.Checked Then
+         Return Nothing
+      ElseIf GlobalRadioButton.Checked Then
+         Return Log_Utils.LogLine.ChatEnum.Global_
+      ElseIf GuildRadioButton.Checked Then
+         Return Log_Utils.LogLine.ChatEnum.Guild
+      ElseIf PartyRadioButton.Checked Then
+         Return Log_Utils.LogLine.ChatEnum.Party
+      ElseIf TradeRadioButton.Checked Then
+         Return Log_Utils.LogLine.ChatEnum.Trade
+      ElseIf WhisperRadioButton.Checked Then
+         Return Log_Utils.LogLine.ChatEnum.WhisperIn
+      Else
+         Return Nothing
+      End If
+   End Function
 End Class
